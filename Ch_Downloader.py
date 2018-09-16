cb87d0739207adcb742fe20f7859aaae9fba85f9
SpyrosRoum <38166823+SpyrosRoum@users.noreply.github.com>
2018-09-16 12:32:02 +0300

    Update Ch_Downloader.py

--- Ch_Downloader.py
+++ Ch_Downloader.py
@@ -1,4 +1,4 @@
-# import sys
+import sys
 from tkinter.filedialog import askdirectory, askopenfilename
 
 from selenium import webdriver
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import time
 
